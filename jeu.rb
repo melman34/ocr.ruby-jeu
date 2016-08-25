a1c34ba7e5dfc22d92e67d64857878503bc8fd10
Yvan Duverger <melman34@hotmail.com>
2016-08-25 23:45:24 +0200

    Réalisation du jeu selon les instructions

--- jeu.rb
+++ jeu.rb
@@ -10,20 +10,33 @@ class Personne
   def info
     # A faire:
     # - Renvoie le nom et les points de vie si la personne est en vie
+    if @points_de_vie > 0
+        return "#{@nom} (#{@points_de_vie}/100pv)"
     # - Renvoie le nom et "vaincu" si la personne a été vaincue
+    else
+        return "(#{@nom} est vaincu)"
+    end
   end
 
   def attaque(personne)
     # A faire:
-    # - Fait subir des dégats à la personne passée en paramètre
     # - Affiche ce qu'il s'est passé
+    puts "#{@nom} attaque #{personne.nom}"
+    # - Fait subir des dégats à la personne passée en paramètre
+    personne.subit_attaque(degats)
   end
 
   def subit_attaque(degats_recus)
     # A faire:
     # - Réduit les points de vie en fonction des dégats reçus
+    @points_de_vie -= degats_recus
     # - Affiche ce qu'il s'est passé
+    puts "#{@nom} subit #{degats_recus} points de dégats!"
     # - Détermine si la personne est toujours en_vie ou non
+    if @points_de_vie <= 0
+        @en_vie = false
+        puts "#{@nom} ne se relèvera pas!!"
+    end
   end
 end
 
@@ -41,19 +54,27 @@ class Joueur < Personne
   def degats
     # A faire:
     # - Calculer les dégats
+    degats = rand(15..50) # défini une valeur entre 15 et 50 aléatoirement
+    degats += @degats_bonus # ajoute le degats bonus
     # - Affiche ce qu'il s'est passé
+    puts "#{@nom} profite de #{@degats_bonus} points de dégats bonus"
+    return degats
   end
 
   def soin
     # A faire:
     # - Gagner de la vie
+    @points_de_vie += rand(20..45) # ajoute entre 20 et 45 points de vie supplémentaire
     # - Affiche ce qu'il s'est passé
+    puts "#{@nom} gagne en points de vie!"
   end
 
   def ameliorer_degats
     # A faire:
     # - Augmenter les dégats bonus
+    @degats_bonus += rand(5..25) # ajoute entre 5 et 25 degats bonus supplémentaires 
     # - Affiche ce qu'il s'est passé
+    puts "#{@nom} gagne en puissance!"
   end
 end
 
@@ -61,6 +82,7 @@ class Ennemi < Personne
   def degats
     # A faire:
     # - Calculer les dégats
+    return rand(3..15) # défini une valeur entre 3 et 15 aléatoirement
   end
 end
 
@@ -84,6 +106,7 @@ class Jeu
   def self.est_fini(joueur, monde)
     # A faire:
     # - Déterminer la condition de fin du jeu
+    joueur.en_vie == false || monde.ennemis_en_vie.size == 0
   end
 end
 
@@ -93,6 +116,11 @@ class Monde
   def ennemis_en_vie
     # A faire:
     # - Ne retourner que les ennemis en vie
+    ennemis_en_vie = []
+    ennemis.each do |ennemi|
+        ennemis_en_vie << ennemi if ennemi.en_vie
+    end
+    return ennemis_en_vie
   end
 end
 
